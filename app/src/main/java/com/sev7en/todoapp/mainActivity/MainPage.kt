2c95044b323a12e6b0064640bfaa379b2af9258e
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-06-28 16:13:27 +0530

    Added SplashFragment and setup the nav_graph navigation component.

---
+++ app/src/main/java/com/sev7en/todoapp/mainActivity/MainPage.kt
@@ -0,0 +1,12 @@
+package com.sev7en.todoapp.mainActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sev7en.todoapp.R
+
+class MainPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
